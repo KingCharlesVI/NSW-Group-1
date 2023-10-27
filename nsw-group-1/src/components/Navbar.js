a947b743fc741d4a99e17a9d223ef1fc34da3ba7
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2023-10-27 19:41:42 +0100

    Update

--- nsw-group-1/src/components/Navbar.js
+++ nsw-group-1/src/components/Navbar.js
@@ -5,12 +5,15 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <h2>NSWG1</h2>
+        <div className="logo-container">
+          <img src="/images/logo.png" className="navbar-logo" alt="Logo" />
+          <h2>NSWG1</h2>
+        </div>
       </div>
       <div className="navbar-right">
         <a href="https://www.youtube.com/channel/UC6HCiNKNSpUJdIdpyDfS4sA" target="_blank" rel="noopener noreferrer">Youtube</a>
         <a href="https://discord.gg/xTGT2auk" target="_blank" rel="noopener noreferrer">Discord</a>
-        <a href="https://nswdg.pro.unitcommander.co.uk/login" target="blank" rel="noopener noreferrer" className="signup-link">SIGNUP</a>
+        <a href="https://nswdg.pro.unitcommander.co.uk/login" target="_blank" rel="noopener noreferrer" className="signup-link">SIGNUP</a>
       </div>
     </nav>
   );
